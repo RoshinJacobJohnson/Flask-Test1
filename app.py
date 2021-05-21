908c2869209ce8b82e0717a9ed35df52184f8540
Roshin Johnson <roshinjohnson@Roshins-MacBook-Pro.local>
2021-05-22 00:17:43 +0530

    21stMAy 5 commit

--- app.py
+++ app.py
@@ -6,11 +6,34 @@ from flask import Flask
 # Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
 app = Flask(__name__)
 
+
+
+
+
+
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    vehicle = request.get_json()
+
+
+    result = {
+        'mpg_prediction': 23
+    }
+    return jsonify(result)
+
+
+
 # Annotation that allows the function to be hit at the specific URL.
 @app.route("/")
 # Generic Python functino that returns "Hello world!"
 def index():
     return "Hello world!"
+
+
+
 
 
 @app.route('/ping', methods=['GET'])
