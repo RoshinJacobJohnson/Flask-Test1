96a31509efc40205eaf7f6702d7cd0c0e343a3d3
Roshin Johnson <roshinjohnson@Roshins-MacBook-Pro.local>
2021-05-22 00:13:19 +0530

    21stMAy 4 commit

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def index():
     return "Hello world!"
 
 
+@app.route('/ping', methods=['GET'])
+def ping():
+    return "Pinging Model!!"
+
+
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
     # Runs the Flask application only if the main.py file is being run.
