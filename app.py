6429743cf195bf17b8ff5b86d17e22588ae54600
Roshin Johnson <roshinjohnson@Roshins-MacBook-Pro.local>
2021-05-23 18:34:59 +0530

    23rdMAy 4 commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 # Import the Flask module that has been installed.
 from flask import Flask
+from flask import jsonify
 
 # Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
 app = Flask(__name__)
