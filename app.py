22d0e0b26265f0301a074651dd6a64816b3a668d
Roshin Johnson <roshinjohnson@Roshins-MacBook-Pro.local>
2021-05-23 18:39:35 +0530

    23rdMAy 5 commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 # Import the Flask module that has been installed.
 from flask import Flask
-from flask import jsonify
+from flask import jsonify,request
 
 # Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
 app = Flask(__name__)
@@ -21,7 +21,7 @@ def add():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    #vehicle = request.get_json()
+    vehicle = request.get_json()
     #vehicle=request.form['vehicle']
 
 
